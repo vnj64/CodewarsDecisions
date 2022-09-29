d4891053dd0f3e77679d80201fcce0e3c214798a
Kamil Khabibullin <vnj644@gmail.com>
2022-09-29 11:49:56 +0300

    Same Case

---
+++ 8lvl/same_case.py
@@ -0,0 +1,9 @@
+def same_case(a, b): 
+    if a.isalpha() and b.isalpha():
+        if (a.islower() and b.islower()) or (a.isupper() and b.isupper()):
+            return 1
+        else:
+            return 0
+    else:
+        return -1
+print(same_case('A', 'G'))
