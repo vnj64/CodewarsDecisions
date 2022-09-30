e3b227a256eca5dfd6a75e66e4ee9443ef175d02
Kamil Khabibullin <vnj644@gmail.com>
2022-09-30 13:25:40 +0300

    Lazy repeater

---
+++ 5lvl/lazy_repeater.py
@@ -0,0 +1,12 @@
+from collections import abc
+
+
+def make_looper(string):
+    global lst
+    lst = list(string)
+    return abc
+
+def abc():
+    poppy = lst.pop(0)
+    lst.append(poppy)
+    return poppy
